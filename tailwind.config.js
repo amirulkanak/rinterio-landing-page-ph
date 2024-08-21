4f9917f3c3cbc548e47339ae178a1a721fee911d
amirulkanak <coder.kanak@gmail.com>
2024-08-22 00:46:20 +0600

    Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      // Custom media queries screen size
+      screens: {
+        tab: '834px',
+        pc: '1170px',
+      },
+      // Custom font family
+      fontFamily: {
+        manrope: ['"Manrope"', 'sans-serif'],
+      },
+      // Custom colors
+      colors: {
+        'clr-gray-5': '#494949',
+        'clr-gray-6': '#212121',
+        'clr-off-white': '#F4F4F4',
+      },
+      backgroundImage: {
+        'hero-bg':
+          'linear-gradient(0deg, #FFF 0%, rgba(255, 255, 255, 0.00) 100%)',
+      },
+    },
   },
   plugins: [require('daisyui')],
 
